3a2ad8d2fddf9e25ac38a0e4bd243a9069cb84a9
zrdzn <zrdzn9@gmail.com>
2026-08-01 15:30:30 +0200

    Add BlockPlaceListener

--- rensai-agents/rensai-agent-paper/src/main/java/dev/rensai/agent/paper/listener/BlockListener.java
+++ rensai-agents/rensai-agent-paper/src/main/java/dev/rensai/agent/paper/listener/BlockListener.java
@@ -6,6 +6,7 @@ import dev.rensai.agent.paper.PaperGameEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.Plugin;
 
 public class BlockListener extends AbstractAgentListener {
@@ -25,6 +26,25 @@ public class BlockListener extends AbstractAgentListener {
                   .putMap("player", mappers.mapPlayer(event.getPlayer()))
                   .put("exp_to_drop", String.valueOf(event.getExpToDrop()))
                   .put("is_drop_items", String.valueOf(event.isDropItems()))
+                  .build();
+
+          return new PaperGameEvent(event.getEventName(), properties);
+        });
+  }
+
+  @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+  public void onBlockPlace(BlockPlaceEvent event) {
+    handleEvent(
+        event,
+        () -> {
+          EventProperties properties =
+              propertyBuilder()
+                  .putMap("block_placed", mappers.mapBlock(event.getBlockPlaced()))
+                  .putMap("block_against", mappers.mapBlock(event.getBlockAgainst()))
+                  .putMap("player", mappers.mapPlayer(event.getPlayer()))
+                  .putMap("item_in_hand", mappers.mapItemStack(event.getItemInHand()))
+                  .put("can_build", String.valueOf(event.canBuild()))
+                  .put("hand", event.getHand().name())
                   .build();
 
           return new PaperGameEvent(event.getEventName(), properties);
